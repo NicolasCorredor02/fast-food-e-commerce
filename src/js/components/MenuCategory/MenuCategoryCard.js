7124c16250fced661ab373159c26bf5f7ad24392
NicolasCorredor02 <nicolas.d02@outlook.com>
2024-11-26 02:33:57 -0500

    style: 💄 Change cursor pointer menu page

--- src/js/components/MenuCategory/MenuCategoryCard.js
+++ src/js/components/MenuCategory/MenuCategoryCard.js
@@ -5,7 +5,7 @@
  */
 export const MenuCategoryCard = (product) => {
   return `
-    <div id="type${product.type}" class="flex flex-col bg-orange shadow-md shadow-black rounded-3xl md:rounded-none md:rounded-es-3xl md:rounded-ee-3xl p-3">
+    <div id="type${product.type}" class="flex flex-col bg-orange shadow-md shadow-black rounded-3xl md:rounded-none md:rounded-es-3xl md:rounded-ee-3xl p-3 cursor-pointer">
         <img src="${product.img}" alt="${product.name}">
         <p class="uppercase text-white font-semibold">${product.type}</p>
     </div>
